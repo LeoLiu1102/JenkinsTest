62eafb55845fc2162e4644a695474af1a5a7b5f7
LeoLiu1102 <leo.l@mundomedia.com>
2018-11-30 15:11:37 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		System.out.println("Hi There! This is a Jenkins test");
 		new File("\\\\10.201.2.98\\c$\\foobar1.txt").createNewFile();
