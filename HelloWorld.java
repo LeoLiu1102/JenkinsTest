5655d8a892ddf7fd56f49b001a7b713d2840bcb6
LeoLiu1102 <leo.l@mundomedia.com>
2018-11-30 14:51:50 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,11 @@
+import java.io.File;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hi There! This is a Jenkins test");
+		new File("\\\\10.201.2.98\\c$\\foobar1.txt").createNewFile();
 
 	}
 
